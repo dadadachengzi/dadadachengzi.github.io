7b8fb49d9a76eca5ed3ed2fe68fc7d77563a1bc9
dadadachengzi <dadadachengzi@yeah.net>
2018-04-28 21:20:12 +0800

    Site updated: 2018-04-28 21:20:10

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 (function IEUpgrade(){
     var ua = window.navigator.userAgent.toLowerCase();
     if (/msie/.test(ua)) {
-        window.location.href = '4fan.html';
+        window.location.href = 'https://dachengzi.cn/4fan.html';
     }
 })();
